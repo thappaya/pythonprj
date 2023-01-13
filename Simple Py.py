24a1c64a017060642741c7c6bed3c7d2e0454dae
Thappaya Wangsantiporn <thappaya.w@gmail.com>
2023-01-13 22:56:22 +0700

    Initial committed

---
+++ Simple Py.py
@@ -0,0 +1,11 @@
+coolcar = {
+    "name": "Dodge Viper GTS",
+    "generation": "Generation V",
+    "engine": "8.4 Liter V10",
+    "horsepower": "640HP",
+    "alternative_branding": "SRT",
+    "induction_type": "Naturally Aspirated",
+}
+coolcar["brand"] = "Chrysler, Dodge"
+coolcar["torque"] = "600ft-lb"
+print(coolcar)
